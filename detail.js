3be1cd42dbb293103d095a7d7be6181e1f31c484
NguyenLe-commits <nguyenlecp2710@gmail.com>
2026-03-01 10:12:17 +0700

    Delete detail.js

--- detail.js
+++
@@ -1,9 +0,0 @@
-const title = localStorage.getItem('diaryTitle');
-  const content = localStorage.getItem('diaryContent');
-  
-  document.getElementById('detail-title').textContent = title;
-  document.getElementById('detail-content').textContent = content;
-
-  function goBack() {
-    window.location.href = "index.html#nhatky";
-  }
